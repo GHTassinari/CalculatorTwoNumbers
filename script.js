7f3d0eea26cdb7f9effffcdbf30cc5294084b059
Guilherme Tassinari <102005103+GHTassinari@users.noreply.github.com>
2023-10-19 17:50:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-let firstNumber = prompt("Inform the first number:")
-let secondNumber = prompt("Inform the second number:")
+let firstNumber = prompt("Inform the first number:");
+let secondNumber = prompt("Inform the second number:");
 
-firstNumber = Number(firstNumber)
-secondNumber = Number(secondNumber)
+firstNumber = Number(firstNumber);
+secondNumber = Number(secondNumber);
 
-const sum = firstNumber + secondNumber
-const sub = firstNumber - secondNumber
-const multi = firstNumber * secondNumber
-const div = firstNumber / secondNumber
-const restDiv = firstNumber % secondNumber
+const sum = firstNumber + secondNumber;
+const sub = firstNumber - secondNumber;
+const multi = firstNumber * secondNumber;
+const div = firstNumber / secondNumber;
+const restDiv = firstNumber % secondNumber;
 
-alert("The sum is: " + sum)
-alert("The subtraction is: " + sub)
-alert("The multiplication is: " + multi)
-alert("The division is: " + div)
-alert("The rest of the division is: " + restDiv)
+alert("The sum is: " + sum);
+alert("The subtraction is: " + sub);
+alert("The multiplication is: " + multi);
+alert("The division is: " + div);
+alert("The rest of the division is: " + restDiv);
 
+if (sum % 2 == 0) {
+  alert(`The sum of the numbers is even: ${sum}`);
+} else {
+  alert(`The sum of the numbers is odd: ${sum}`)
+}
+
+if (firstNumber==secondNumber){
+  alert("The first, and second numbers, are the same!")
+} else{
+  alert("The numbers are different.")
+}
